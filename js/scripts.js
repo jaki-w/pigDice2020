e06ff3a80fff5e73d996c387a62c36325f78faa7
Jaki Widby <jaki.widby@gmail.com>
2020-08-26 10:45:56 -0700

    create initial specs, create game and player constructors, FIRST SPEC COMPLETE
    Co-Authered-by: Amanda Otero <mandystar018@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+//Business Logic
+function Game() {
+  this.players = [],
+  this.currentId = 0;
+}
+
+function Player() {
+  this.turnScore = turnScore,
+  this.score = score
+}
+
+function generateRandomNumber() {
+  let randomNumber = Math.floor(Math.random() * 6) + 1;
+  return randomNumber;
+}
+
+
+// A random number between 1 & 6 is generated
+
+//if a 1 is rolled, turn score is 0
+
+//player is switched when 1 is rolled
+
+// current score is compiled in turn score for multiple rolls
+
+//turn score is added to total score at turn end
+
+//player is switched when current player holds
+
+//game ends when a player's score reaches 100
+
+
+//Object: Game: Properties: players, currentId, 
+
+//Object: Player: Properties: turnScore, totalScore
+
+//Prototype: Game/ assignId
+
+
+
+
+
