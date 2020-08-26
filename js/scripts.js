503b91efbd96a1d67b1f0d9a8491c198f35f1900
Jaki Widby <jaki.widby@gmail.com>
2020-08-26 11:26:11 -0700

    create skeleton html page with gameBoard div, scoreBoard div, and rules div
    Co-Authered-by: Amanda Otero <mandystar018@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -6,19 +6,36 @@ function Game() {
 
 function Player() {
   this.turnScore = turnScore,
-  this.score = score
+  this.score = score;
 }
 
+// A random number between 1 & 6 is generated 
 function generateRandomNumber() {
   let randomNumber = Math.floor(Math.random() * 6) + 1;
   return randomNumber;
 }
 
-
-// A random number between 1 & 6 is generated
+// Number rolled is added to player turn total
 
 //if a 1 is rolled, turn score is 0
 
+Player.prototype.rollDice = function() {
+  let roll = generateRandomNumber();
+
+  if (roll === 1) {
+    this.turnScore = 0;
+    // showRoll();
+    // turnEnd();
+  } 
+}
+
+// function showRoll() {
+//   $("#currentRoll").text(roll);
+// }
+
+// function turnEnd(name) {
+//   $("#")
+// }
 //player is switched when 1 is rolled
 
 // current score is compiled in turn score for multiple rolls
